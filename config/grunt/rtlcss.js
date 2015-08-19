a8fbe832f7e11e46603ab60bf2b079d1db21055a
robneu <rob@wpsitecare.com>
2015-08-19 15:36:28 -0500

    RTL all the things!

--- config/grunt/rtlcss.js
+++ config/grunt/rtlcss.js
@@ -1,21 +1,20 @@
 // https://github.com/MohammadYounes/grunt-rtlcss
 module.exports = {
-	theme: {
-		options: {
-			swapLeftRightInUrl: false,
-			swapLtrRtlInUrl: false,
-			autoRename: false,
-			preserveDirectives: true
-		},
-		files: [
-			{
-				src: '<%= paths.tmp %><%= pkg.nameDashed %>.css',
-				dest: '<%= paths.tmp %><%= pkg.nameDashed %>-rtl.css'
-			},
-			{
-				src: '<%= paths.tmp %>admin-<%= pkg.nameDashed %>.css',
-				dest: '<%= paths.tmp %>admin-<%= pkg.nameDashed %>-rtl.css'
-			}
-		]
+	options: {
+		swapLeftRightInUrl: false,
+		swapLtrRtlInUrl: false,
+		autoRename: false,
+		preserveDirectives: true
+	},
+	plugin: {
+		expand: true,
+		cwd: '<%= paths.tmp %>',
+		src: [
+			'*.css',
+			'!*.min.css'
+		],
+		dest: '<%= paths.tmp %>',
+		ext: '.css',
+		extDot: 'last'
 	}
 };
