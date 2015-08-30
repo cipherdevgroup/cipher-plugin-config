2d904f335659435ef127245ff70721231d5884f4
robneu <rob@wpsitecare.com>
2015-08-30 14:40:23 -0500

    Spacing and a URL fix

--- config/grunt/potomo.js
+++ config/grunt/potomo.js
@@ -1,11 +1,11 @@
-// https://github.com/axisplugins/grunt-potomo
+// https://github.com/axisthemes/grunt-potomo
 module.exports = {
 	release: {
 		files: [
 			{
 				expand: true,
 				cwd: '<%= paths.assets %><%= paths.languages %>',
-				src: ['*.po'],
+				src: [ '*.po' ],
 				dest: '<%= paths.plugin%><%= paths.languages %>',
 				ext: '.mo',
 				nonull: true,
