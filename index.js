f6fbb57129535aa79c1df2537be93388b600c23a
robneu <rob@wpsitecare.com>
2015-09-01 19:22:38 -0500

    Flattened things out a bit

--- index.js
+++ index.js
@@ -3,61 +3,63 @@ module.exports = function() {
 	'use strict';
 
 	var path = require( 'path' ),
-		project = null;
+		project = {};
 
-	project = {
-		paths: {
-			config:    'config/',
-			assets:    'assets/',
-			languages: 'languages/',
-			release:   'release/',
-			tmp:       'tmp/',
-			jsSrc:     'js/src/',
-			cssSrc:    'css/src/',
-			fontSrc:   'font/src/',
-			imagesSrc: 'images/src/',
-			bower:     'bower/',
-			grunt:     'config/grunt/',
-			tasks:     'config/grunt/tasks/',
-			global: {
-				config: path.join( __dirname, 'config/' ),
-				grunt:  path.join( __dirname, 'config/grunt/' ),
-				tasks:  path.join( __dirname, 'config/grunt/tasks/' )
-			}
-		},
-		taskMap: {
-			addtextdomain: 'grunt-wp-i18n',
-			makepot:       'grunt-wp-i18n',
-			usebanner:     'grunt-banner',
-			wpcss:         'grunt-wp-css'
-		}
+	project.paths = {
+		config:    'config/',
+		assets:    'assets/',
+		languages: 'languages/',
+		release:   'release/',
+		tmp:       'tmp/',
+		jsSrc:     'js/src/',
+		cssSrc:    'css/src/',
+		fontSrc:   'font/src/',
+		imagesSrc: 'images/src/',
+		bower:     'bower/',
+		grunt:     'config/grunt/',
+		tasks:     'config/grunt/tasks/'
+	};
+
+	project.paths.global = {
+		config: path.join( __dirname, 'config/' ),
+		grunt:  path.join( __dirname, 'config/grunt/' ),
+		tasks:  path.join( __dirname, 'config/grunt/tasks/' )
+	};
+
+	project.taskMap = {
+		addtextdomain: 'grunt-wp-i18n',
+		makepot:       'grunt-wp-i18n',
+		usebanner:     'grunt-banner',
+		wpcss:         'grunt-wp-css'
 	};
 
 	project.files = {
-		js: [
-			project.paths.jsSrc + '**/*.js',
-			'!' + project.paths.jsSrc + '**/*.min.js',
-			'!' + project.paths.jsSrc + 'vendor/'
-		],
 		scss:   project.paths.cssSrc  + '**/*.scss',
-		config: project.paths.config  + '**/*.js',
-		php: [
-			'*.php',
-			'**/*.php',
-			'!.git/**',
-			'!.sass-cache/**',
-			'!assets/**',
-			'!css/**',
-			'!release/**',
-			'!fonts/**',
-			'!images/**',
-			'!js/**',
-			'!languages/**',
-			'!bower_components/**',
-			'!node_modules/**',
-			'!tmp/**'
-		]
+		config: project.paths.config  + '**/*.js'
 	};
+
+	project.files.js = [
+		project.paths.jsSrc + '**/*.js',
+		'!' + project.paths.jsSrc + '**/*.min.js',
+		'!' + project.paths.jsSrc + 'vendor/'
+	];
+
+	project.files.php = [
+		'*.php',
+		'**/*.php',
+		'!.git/**',
+		'!.sass-cache/**',
+		'!assets/**',
+		'!css/**',
+		'!release/**',
+		'!fonts/**',
+		'!images/**',
+		'!js/**',
+		'!languages/**',
+		'!bower_components/**',
+		'!node_modules/**',
+		'!tmp/**'
+	];
 
 	return project;
 };
