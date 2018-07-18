b9555448e10a1042d191a9daf04282873f6f66e2
robneu <yo@robneu.com>
2018-07-18 18:34:01 -0400

    Try some generic defaults for the sass config

--- config/grunt/sass.js
+++ config/grunt/sass.js
@@ -7,5 +7,14 @@ module.exports = {
 		sourceMap: false,
 		lineNumbers: false,
 		outputStyle: 'expanded'
+	},
+	plugin: {
+		expand: true,
+		cwd: '<%= paths.cssSrc %>',
+		src: [
+			'*.scss'
+		],
+		dest: '<%= paths.cssDist %>',
+		ext: '.css'
 	}
 };
